351a81f32c020d89532954dab32937bf7859ac15
mankouuu <mankoulani.fofanah@std.heh.be>
2024-09-24 09:47:12 +0200

    le_main

---
+++ main.py
@@ -0,0 +1,15 @@
+liste = [1.0 ,2.0, 3.0]
+print(liste)
+
+
+
+#somme = sum(liste)
+'''print('somme = ' , somme)
+nbr_ele = len(liste)
+print(nbr_ele)
+moyenne = somme /3
+print(moyenne)
+mini = min(liste)
+print(mini)
+maxi = max(liste)
+print(maxi)'''
